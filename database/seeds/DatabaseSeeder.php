0de85950cb1abd691de93ce761d0bb0bac5dae26
Damar Permadany <damarp2017@gmail.com>
2019-07-02 10:43:50 +0700

    clear all faker seeder except admin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,102 +20,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Factory::create();
-        // $this->call(UsersTableSeeder::class);
         DB::table('admins')->insert([
             'image' => 'default.png',
             'name' => 'Administrator',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('12345678'),
         ]);
-
-        // DB::table('users')->insert([
-        //     'image' => 'default.png',
-        //     'name' => 'User1',
-        //     'email' => 'user1@gmail.com',
-        //     'password' => Hash::make('12345678'),
-        // ]);
-
-        // DB::table('users')->insert([
-        //     'image' => 'default.png',
-        //     'name' => 'User2',
-        //     'email' => 'user2@gmail.com',
-        //     'password' => Hash::make('12345678'),
-        // ]);
-
-        foreach (range(1,50) as $i) {
-            Food::create([
-                'image' => 'default.png',
-                'name' => 'Food Name ' . $i,
-                'price' => $faker->randomElement([10000, 20000, 30000, 5000]),
-                'description' => $faker->text(200),
-                'category' => $faker->randomElement(['Type A', 'Type B', 'Type C']),
-                'seller' => $faker->name,
-                'created_at' => $faker->dateTime(),
-                'updated_at' => $faker->dateTime(),
-            ]);
-        }
-
-        foreach (range(1,50) as $i) {
-            Medicine::create([
-                'image' => 'default.png',
-                'name' => 'Medicine Name ' . $i,
-                'price' => $faker->randomElement([10000, 20000, 30000, 5000]),
-                'description' => $faker->text(200),
-                'category' => $faker->randomElement(['Type A', 'Type B', 'Type C']),
-                'seller' => $faker->name,
-                'created_at' => $faker->dateTime(),
-                'updated_at' => $faker->dateTime(),
-            ]);
-        }
-
-        foreach (range(1,50) as $i) {
-            WashingAndSpa::create([
-                'image' => 'default.png',
-                'name' => 'Washing and Spa Name ' . $i,
-                'description' => $faker->text(300),
-                'street' => $faker->streetAddress,
-                'districts' => $faker->city,
-                'city' => $faker->state,
-                'created_at' => $faker->dateTime(),
-                'updated_at' => $faker->dateTime(),
-            ]);
-        }
-
-        foreach (range(1,50) as $i) {
-            BuyingAnimal::create([
-                'image' => 'default.png',
-                'name' => $faker->firstName,
-                'description' => $faker->text(300),
-                'address' => $faker->address,
-                'phone' => $faker->phoneNumber,
-                'created_at' => $faker->dateTime(),
-                'updated_at' => $faker->dateTime(),
-            ]);
-        }
-
-        foreach (range(1,50) as $i) {
-            PetShop::create([
-                'image' => 'default.png',
-                'name' => $faker->name . " Shop",
-                'description' => $faker->text(300),
-                'street' => $faker->streetAddress,
-                'districts' => $faker->city,
-                'city' => $faker->state,
-                'created_at' => $faker->dateTime(),
-                'updated_at' => $faker->dateTime(),
-            ]);
-        }
-
-        foreach (range(1,50) as $i) {
-            Community::create([
-                'image' => 'default.png',
-                'name' => $faker->firstName . "Community",
-                'description' => $faker->text(300),
-                'leader' => $faker->name,
-                'address' => $faker->address,
-                'phone' => $faker->phoneNumber
-            ]);
-        }
     }
 }
