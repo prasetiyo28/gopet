fed3e14c9dc102ad67011d499f9d22b4149b6574
Damar Permadany <damarp2017@gmail.com>
2019-07-17 07:26:08 +0700

    remove unnecessary api routes

--- routes/api.php
+++ routes/api.php
@@ -9,19 +9,6 @@ use Illuminate\Http\Request;
 Route::get('item', 'Api\ItemController@index');
 Route::get('item/{id}', 'Api\ItemController@show');
 
-Route::get('foods', 'Api\FoodController@index');
-Route::get('foods/{id}', 'Api\FoodController@show');
-Route::post('foods', 'Api\FoodController@store');
-Route::post('foods/{id}', 'Api\FoodController@update');
-Route::delete('foods/{id}', 'Api\FoodController@destroy');
-
-
-Route::get('medicine', 'Api\MedicineController@index');
-Route::get('medicine/{id}', 'Api\MedicineController@show');
-Route::post('medicine', 'Api\MedicineController@store');
-Route::post('medicine/{id}', 'Api\MedicineController@update');
-Route::delete('medicine/{id}', 'Api\MedicineController@destroy');
-
 Route::get('washing-and-spa', 'Api\WashingAndSpaController@index');
 Route::get('washing-and-spa/{id}', 'Api\WashingAndSpaController@show');
 Route::post('washing-and-spa', 'Api\WashingAndSpaController@store');
